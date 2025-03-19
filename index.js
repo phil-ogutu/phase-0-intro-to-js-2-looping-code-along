70a1b0d318a13bb136b19c0981b72231c43a57ae
phil-ogutu <phil.shifwoko@gmail.com>
2025-03-19 20:28:19 +0300

    Looping

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let names = ["John", "James", "Alice"]
+function writeCards(names){
+    let messages = []
+    for(let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return messages
+}
+writeCards(names)
+
+function countDown(number){
+    while(number>=0){
+        console.log(number)
+        number--
+    }
+}
+
+countDown(10)
